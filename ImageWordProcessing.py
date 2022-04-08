4a09142f13c6dc5375d10ad213e8efd4cb00b401
11036013 <11036013@ntub.edu.tw>
2022-04-08 22:49:48 +0800

    新增使用的套件之絕對位置

--- ImageWordProcessing.py
+++ ImageWordProcessing.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from PIL import Image
 import pytesseract
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 img_name = 'C:/chashin/2技/python文字辨識/demo-en-us.png'
 img = Image.open(img_name)
 text = pytesseract.image_to_string(img, lang='eng')
