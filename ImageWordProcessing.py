0ef5a8657f1ce8dc9d8065a35853f8ae8d137490
11036013 <11036013@ntub.edu.tw>
2022-04-08 22:58:44 +0800

    增加中文判斷及測試圖片

--- ImageWordProcessing.py
+++ ImageWordProcessing.py
@@ -2,8 +2,9 @@
 from PIL import Image
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
-img_name = 'C:/chashin/2技/python文字辨識/demo-en-us.png'
+#img_name = 'C:/chashin/2技/python文字辨識/demo-en-us.png'
+img_name = 'C:/chashin/2技/python文字辨識/002-zh-cht.png'
 img = Image.open(img_name)
-text = pytesseract.image_to_string(img, lang='eng')
+text = pytesseract.image_to_string(img, lang='chi_tra+chi_sim+eng')
 print(text)
 print("text")
