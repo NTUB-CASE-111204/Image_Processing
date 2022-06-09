1f214260bb70e90529cc7c8f1384ce1df2cc5456
11036013 <11036013@ntub.edu.tw>
2022-06-09 22:43:30 +0800

    增加更多的品牌測試

--- ImageWordProcessing.py
+++ ImageWordProcessing.py
@@ -3,8 +3,12 @@ from PIL import Image
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 #img_name = 'C:/chashin/2技/python文字辨識/demo-en-us.png'
-img_name = 'C:/chashin/2技/python文字辨識/002-zh-cht.png'
+#img_name = 'C:/chashin/2技/python文字辨識/002-zh-cht.png'
+#img_name = 'C:/chashin/2技/python文字辨識/goro4.png'
+#img_name = 'C:/chashin/2技/python文字辨識/goro2.jpg'
+#img_name = 'C:/chashin/2技/python文字辨識/PATTIS.png'
+img_name = 'C:/chashin/2技/python文字辨識/IRITA.png'
 img = Image.open(img_name)
-text = pytesseract.image_to_string(img, lang='chi_tra+chi_sim+eng')
+text = pytesseract.image_to_string(img, lang='eng+chi_tra+chi_sim')
 print(text)
-print("text")
+#print("text")
