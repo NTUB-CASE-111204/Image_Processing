00d0c0f36f0a747189365faad1d317861156b2e3
11036013 <11036013@ntub.edu.tw>
2022-07-14 18:52:11 +0800

    試翻譯英文至中文

--- ImageWordProcessing.py
+++ ImageWordProcessing.py
@@ -4,8 +4,8 @@ from PIL import Image
 from googletrans import Translator
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
-#img_name = 'C:/chashin/2技/python文字辨識/demo-en-us.png'
-img_name = 'C:/chashin/2技/python文字辨識/002-zh-cht.png'
+img_name = 'C:/chashin/2技/python文字辨識/demo-en-us.png'
+#img_name = 'C:/chashin/2技/python文字辨識/002-zh-cht.png'
 #img_name = 'C:/chashin/2技/python文字辨識/goro4.png'
 #img_name = 'C:/chashin/2技/python文字辨識/goro2.jpg'
 #img_name = 'C:/chashin/2技/python文字辨識/PATTIS.png'
@@ -16,9 +16,8 @@ img = Image.open(img_name)
 text = pytesseract.image_to_string(img, lang='eng+chi_tra')
 print(text)
 #print("text")
-text="測試"
 
 translator = Translator()
-result = translator.translate(text, dest='en').text
+result = translator.translate(text, dest='zh-TW').text
 print(result)
 #translator = Translator(service_urls=['translate.google'])
