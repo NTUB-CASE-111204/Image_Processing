bc695b07598185043096d017e101168e92fbbd36
11036013 <11036013@ntub.edu.tw>
2022-07-14 19:03:33 +0800

    測試另一張LOGO

--- ImageWordProcessing.py
+++ ImageWordProcessing.py
@@ -4,13 +4,13 @@ from PIL import Image
 from googletrans import Translator
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
-img_name = 'C:/chashin/2技/python文字辨識/demo-en-us.png'
+#img_name = 'C:/chashin/2技/python文字辨識/demo-en-us.png'
 #img_name = 'C:/chashin/2技/python文字辨識/002-zh-cht.png'
 #img_name = 'C:/chashin/2技/python文字辨識/goro4.png'
 #img_name = 'C:/chashin/2技/python文字辨識/goro2.jpg'
 #img_name = 'C:/chashin/2技/python文字辨識/PATTIS.png'
 #img_name = 'C:/chashin/2技/python文字辨識/IRITA.png'
-#img_name = 'C:/chashin/2技/python文字辨識/chih.jpg'
+img_name = 'C:/chashin/2技/python文字辨識/chih.jpg'
 #img_name = 'C:/chashin/2技/python文字辨識/ONE.jpg'
 img = Image.open(img_name)
 text = pytesseract.image_to_string(img, lang='eng+chi_tra')
