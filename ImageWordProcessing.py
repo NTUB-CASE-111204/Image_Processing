ad52568c916edad0ede361a41241edf5e11a81f9
11036013 <11036013@ntub.edu.tw>
2022-08-26 18:04:04 +0800

    訓練以及測試用的程式碼、圖片等...

--- ImageWordProcessing.py
+++ ImageWordProcessing.py
@@ -6,16 +6,20 @@ import pytesseract
 import cv2
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 #img_name = 'C:/chashin/2技/python文字辨識/demo-en-us.png'
-img_name = 'C:/chashin/2技/python文字辨識/002-zh-cht.png'
-#img_name = 'C:/chashin/2技/python文字辨識/goro4.png'
+#img_name = 'C:/chashin/2技/python文字辨識/002-zh-cht.png'
+#img_name = 'C:/chashin/2技/python文字辨識/goro4-1.jpg'
 #img_name = 'C:/chashin/2技/python文字辨識/goro2.jpg'
 #img_name = 'C:/chashin/2技/python文字辨識/PATTIS.png'
 #img_name = 'C:/chashin/2技/python文字辨識/IRITA.png'
-#img_name = 'C:/chashin/2技/python文字辨識/chih.jpg'
+img_name = 'C:/chashin/2技/python文字辨識/chih.jpg'
 #img_name = 'C:/chashin/2技/python文字辨識/ONE.jpg'
 img = Image.open(img_name)
-text = pytesseract.image_to_string(img, lang='eng+chi_tra')
-print(text)
+text = pytesseract.image_to_string(img, lang='normal')
+print("自培訓" + text)
+text = pytesseract.image_to_string(img, lang='eng')
+print("英文" + text)
+text = pytesseract.image_to_string(img, lang='chi_tra')
+print("繁中" + text)
 #print("text")
 
 #translator = Translator()
